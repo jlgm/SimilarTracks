8d9d61f93c2b7ae65cc20fb600202e3ca43cc537
jlgm <jlgm@cin.ufpe.br>
2017-01-24 23:44:28 -0300

    updating README and sending picture

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SimilarTracks
 Create a list of similar tracks (data from last.fm)
+
+Demo at: http://www.cin.ufpe.br/~jlgm/SimilarTracks/
--- app/track.js
+++ app/track.js
@@ -12,7 +12,7 @@ angular.module('trackApp', [])
     };
 
     trackList.getTracks = function() {
-        $http.get("https://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=" + trackList.artist + "&track=" + trackList.track + "&api_key=ed77b11b1f0965db65c3144cbffaf27b&format=json").then(function(response) {
+        $http.get("http://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=" + trackList.artist + "&track=" + trackList.track + "&api_key=ed77b11b1f0965db65c3144cbffaf27b&format=json").then(function(response) {
             console.log(response);
             for(var i = 0; i < 50; i++) {
                 trackList.tracks.push({text:response.data.similartracks.track[i].artist.name + " - " + response.data.similartracks.track[i].name, done:false});
